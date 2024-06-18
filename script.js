9611485097c3251caa1a8a51d969d8289508ce6d
Kate Boyd <kate.boyd@stu.ocsb.ca>
2024-06-17 23:25:36 -0400

    final

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
  * Description: Basic game over Minesweeper on an 8 x 8 grid with 7 mines
  *
  * Author: Kate Boyd
+ * In the words of Mme Konan: Lord have mercy...
  */
 
 'use strict';
@@ -27,31 +28,28 @@ let clicky = 1;
 let yx;
 let checkin = [];
 let done = true;
-let lose = 0;
-let available = new Array()
-let already = false;
+let winny = 0;
 let timesec = 0
 let timemin = 0
 let clock;
+let end = false;
 
 class Square {
   colour = [0, 0, 0];
   near = 0;
   mine = "o";
-  flag = "o";
-  uncovered = false
+  uncovered = false;
 
-  constructor(colour, near, mine, flag) {
+  constructor(colour, near, mine, uncovered) {
     this.colour = colour;
     this.near = near;
     this.mine = mine;
-    this.flag = flag;
+    this.uncovered = uncovered;
   }
 }
 
 // Setup the scene (runs first)
 function setup() {
-
   cvs = createCanvas(CVS_WIDTH, CVS_HEIGHT);
 
   // Initialize the grid to all black squares
@@ -93,57 +91,43 @@ function draw_grid(x, y) {
       fill(...grid[row][col].colour);
       rect(col*width + x_buffer, row*height + y_buffer, width, height);
 
-      /*
-      // Flag
-      if (grid[row][col].flag == "F") {
-        textAlign(CENTER, CENTER);
-        fill("red");
-        text(grid[row][col].mine, (col*width + x_buffer)+width/2, (row*height + y_buffer)+width/2);
+      if (grid[row][col].uncovered == true){
+        grid[row][col].colour = [255,255,255];
+        if (grid[row][col].near > 0) {
+          textAlign(CENTER, CENTER);
+          if(grid[row][col].near == 1) fill ("red")
+          if(grid[row][col].near == 2) fill ("blue")
+          if(grid[row][col].near == 3) fill ("green")
+          if (grid[row][col].near > 3)fill ("purple")
+          text(grid[row][col].near, (col*width + x_buffer)+width/2, (row*height + y_buffer)+width/2);
+        }
       }
-
-      */
-      //--------------------------------------------------------------------
-      // Troubleshooting, get rid of later
-      // Write the mines of the square in the center of it
-      if (grid[row][col].mine == "m") {
-        textAlign(CENTER, CENTER);
-        fill("yellow");
-        text(grid[row][col].mine, (col*width + x_buffer)+width/2, (row*height + y_buffer)+width/2);
+      // Write the mines of the square in the center of it when game is won/lost
+      if (end) {
+        clearTimeout(clock)
+        if (grid[row][col].mine == "m"){
+          textAlign(CENTER, CENTER);
+          fill("yellow");
+          text(grid[row][col].mine, (col*width + x_buffer)+width/2, (row*height + y_buffer)+width/2);
+        }
       }
-
-      // Write the mines of the square in the center of it
-      if (grid[row][col].near > 0) {
-        textAlign(CENTER, CENTER);
-        if(grid[row][col].near == 1) fill ("red")
-        if(grid[row][col].near == 2) fill ("blue")
-        if(grid[row][col].near == 3) fill ("green")
-        if (grid[row][col].near > 3)fill ("purple")
-        text(grid[row][col].near, (col*width + x_buffer)+width/2, (row*height + y_buffer)+width/2);
-      }
-
-      //--------------------------------------------------------------------
     }
   }
 }
 
-
-
 function mouseClicked(){
   let x = Math.floor((mouseX/100)/0.5);
   let y = Math.floor((mouseY/100)/0.5);
-
 
   // First click, generating grid with mines and math
   if (mouseButton == LEFT && clicky == 1){
     document.getElementById("zeroTime").hidden = true
     document.getElementById("clock").hidden = false
 
-
     // Clock, counts up every second
     clock = setInterval(countUp, 1000)
 
     // Highlight first click
-    grid[y][x].colour = [255,255,255]
     grid[y][x].uncovered = true;
 
     // Mines
@@ -206,106 +190,53 @@ function mouseClicked(){
         //Assuming that location is part of the grid
         if (y + checky >= 0 && y + checky < 8 && x + checkx >= 0 && x + checkx < 8){
           grid[y + checky][x + checkx].uncovered = true;
-          grid[y + checky][x + checkx].colour = [255,255,255];
-          if (y + checky != y && x + checkx != x) available.push([y + checky, x + checkx]);
-        }
-      }
-    }
-
-    console.log("Just finished first openings so here's available = " + available)
-
-    //something wrong with my checking for repeats in available
-    // Click check was done, now checking how far it can expand
-
-    for (let checky = -1; checky <= 1; checky++){
-      for (let checkx = -1; checkx <= 1; checkx++){
-        for (let i = 0; i < available.length; i++){
-
-          //Assuming that around open location is part of the grid
-          if (available[i][0] + checky >= 0 && available[i][0]+ checky < 8 && available[i][1] + checkx >= 0 && available[i][1] + checkx < 8){
-
-            //something here???
-            //availcheck isnt actually used in this loop lmao what am i doing
-            // Loop through availables to assure no repeats
-            for (let availCheck = 0; availCheck < available.length; availCheck++){
-              if(available[availCheck][0] + checky == available[i][0] && available[availCheck][1] + checkx == available[availCheck][1]){
-               already = true
-              }
-            }
-
-
-            // If it's mineless and not stored before then uncover
-            if (grid[available[i][0] + checky][available[i][1] + checkx].mine != "m" && already == false){
-
-              grid[available[i][0] + checky][available[i][1] + checkx].uncovered = true;
-              grid[available[i][0] + checky][available[i][1] + checkx].colour = [255,255,255];
-
-              if (grid[available[i][0] + checky][available[i][1] + checkx].near == 0){
-                available.push([available[i][0] + checky, available[i][1] + checkx]);
-              }
-            }
-          }
         }
       }
     }
   }
 
   // New clicks
-  if (clicky > 1 && grid[y][x].flag == "o"){
-
+  if (clicky > 1){
 
     // If the click is mine, lose
     if (grid[y][x].mine == "m"){
-      document.getElementById("lose").hidden = true;
-
-      // Can no longer alter the grid
-      clicky = undefined;
-
-      //Stop the clock
-      timesec = 0;
-      timemin = 0;
-      clearTimeout(clock)
+      document.getElementById("lose").hidden = false;
+      end = true;
     }
 
+    // Check open spots around click
+    for (let checky = -1; checky <= 1; checky++){
+      for (let checkx = -1; checkx <= 1; checkx++){
+        //Assuming that location is part of the grid and not mines
+        if (y + checky >= 0 && y + checky < 8 && x + checkx >= 0 && x + checkx < 8 && grid[y + checky][x + checkx].mine == "o"){
+          grid[y + checky][x + checkx].uncovered = true;
+        }
+      }
+    }
 
     // Check if in total all the uncovered squares AREN'T mines
     for (let runy = 0; runy < ROWS; runy++){
       for (let runx = 0; runx < COLS; runx++){
-        // If the spot is uncovered but there's not mine to it
-        if(grid[runy][runx].uncovered == false && grid[runy][runx].mine == "o"){
-          lose++
+        // If the spot is covered and there's no mine you count up
+        if(grid[runy][runx].uncovered == true && grid[runy][runx].mine == "o"){
+          winny++
         }
       }
     }
 
     //If you won, show win, stop timer
-    if (lose == 0){
+    if (winny == 57){
       document.getElementById("win").hidden = false;
-
-      // Can no longer alter the grid
-      clicky = undefined
-
-      // Stop the timer also
-      timesec = 0;
-      timemin = 0;
-      clearTimeout(clock)
-
+      end = true;
     }
-
-  }
-
-  // Flagging on right click
-  if (mouseButton == RIGHT){
-    console.log('jfidsjafpdaf')
   }
 
   clicky++
-  lose = 0;
-  console.log("recent click coords = " + y,x)
-  console.log('done click')
-  console.log(" ")
+  winny = 0;
 }
 
+// HELPER FUNCTIONS
+//---------------------------------------------------------
 // Helper function for first mine generated
 function newMine(y,x){
   done = true;
@@ -331,10 +262,12 @@ function restart(){
   clicky = 1;
   checkin = []
   done = true;
-  lose = 0;
-  already = false;
+  winny = 0;
   timesec = 0;
   timemin = 0;
+  end = false;
+  document.getElementById("win").hidden = true;
+  document.getElementById("lose").hidden = true;
   clearTimeout(clock)
   setup()
 }
@@ -348,12 +281,3 @@ function countUp(){
 
   document.getElementById("clock").innerText = "Timer " + timemin + ":"  + timesec
 }
-
-
-// working on displaying the flagging ish maybe push to later
-// need kill when mine is left clicked (future clicks)
-// algorithm to open up that can be called upon for all #s of left clicks
-// check how to win (when uncovered squares are all mine == o)
-//problems with rightclick never registering
-
-//try making things private
